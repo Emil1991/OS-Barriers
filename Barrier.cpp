97b872ebbb9df0d7c393376364878d97d9a5b723
Emil1991 <emilgulayev@gmail.com>
2020-01-13 15:03:57 +0000

    init cpp

--- Barrier.cpp
+++ Barrier.cpp
@@ -1,4 +1,17 @@
-//
-// Created by Emil & Dennis on 1/5/20.
-//
+#include "Barrier.h"
 
+Barrier::Barrier(unsigned int num_of_threads){
+
+}
+
+unsigned int Barrier::waitingThreads(){
+
+}
+
+void  Barrier::wait(){
+
+}
+
+Barrier::~Barrier(){
+
+}
--- Barrier.h
+++ Barrier.h
@@ -10,6 +10,10 @@ public:
 	unsigned int waitingThreads();
     ~Barrier();
 
+    sem_t * mutex;
+    sem_t* barrier1;
+    sem_t* barrier2;
+
 	// TODO: define the member variables
 	// Remember: you can only use semaphores!
 };
